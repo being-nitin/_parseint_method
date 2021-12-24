57b95a243ce0e8c6fec6fcefb25a02891c52095a
Nitin <nsinghboss02@gmail.com>
2021-12-24 23:21:36 +0530

    Initial commit

---
+++ src/com/codechef/Main.java
@@ -0,0 +1,16 @@
+package com.codechef;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+     java program to covert string into integer, and string to integer:
+	 */
+    Scanner sc = new Scanner(System.in);
+    String s = sc.next();
+    Integer.parseInt(s);
+        System.out.println(s);
+
+
+    }
+}
